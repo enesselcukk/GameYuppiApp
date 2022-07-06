e072a1226a87f6756b29ae7c81c825b728fa6e0b
enes <enesselcu25@gmail.com>
2022-07-06 12:10:24 +0300

    first commit

--- app/src/main/java/com/eneselcuk/gameyuppiapp/GameApplication.kt
+++ app/src/main/java/com/eneselcuk/gameyuppiapp/GameApplication.kt
@@ -3,7 +3,6 @@ package com.eneselcuk.gameyuppiapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import kotlin.properties.Delegates
 
 @HiltAndroidApp
 class GameApplication : Application()
