efe0c7cee13d4b92e0fe27728a5e05b7f4b7a1c7
enes <enesselcu25@gmail.com>
2022-07-06 12:04:14 +0300

    first commit

--- app/src/main/java/com/eneselcuk/gameyuppiapp/domain/repository/GameRepos.kt
+++ app/src/main/java/com/eneselcuk/gameyuppiapp/domain/repository/GameRepos.kt
@@ -6,15 +6,10 @@ import com.eneselcuk.gameyuppiapp.util.NetworkResult
 import kotlinx.coroutines.flow.Flow
 
 interface GameRepos {
-
     suspend fun getStories(): Flow<NetworkResult<Stores>>
     suspend fun deails(
         storeID: String,
         lowerPrice: String? = null,
         upperPrice: String? = null
     ): Flow<NetworkResult<Deails>>
-
-//    suspend fun signUp(email: String, password: String): Flow<NetworkResult<Fires>>
-
-
 }
